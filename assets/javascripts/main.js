cb5e117cf74dc5b1799b06d3c3aa18026368ba0c
Praffulsh <kumarprafful7761@gmail.com>
2024-09-12 22:45:58 +0530

    loader

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -304,7 +304,7 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
+  const preloader = document.querySelector('.preloader');
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove();
